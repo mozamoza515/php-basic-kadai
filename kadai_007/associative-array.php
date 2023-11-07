9af6583cae4fa8c2a6e4982540c993296aefe1ec
mozamoza515 <135591897+mozamoza515@users.noreply.github.com>
2023-11-07 23:32:00 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+   <meta charset="UTF-8">
+   <title>PHPの基礎を学ぼう8章課題</title>
+</head>
+ 
+<body>
+   <p>
+     <?php
+     //連想配列に値を代入する
+     $array_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+     //連想配列の値を出力する
+     print_r($array_data);
+
+     ?>
+   </p>
+</body>
+
+</html>
